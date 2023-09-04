454a5132ca039f5ed7692ab13dbc297b5ff944a3
Kush Vasaniya <vasaniyakush@gmail.com>
2023-09-04 12:01:48 +0530

    Update UsersList.js

--- components/UsersList.js
+++ components/UsersList.js
@@ -1,13 +1,11 @@
 import { useMemo } from 'react';
 
 function UsersList({ users, startingLetter }) {
-  const filteredUsers = useMemo(() => {
-    return users.filter(user => user.name.startsWith(startingLetter));
-  }, [users, startingLetter]);
+  
 
   return (
     <ul>
-      {filteredUsers.map(user => <li key={user.id}>{user.name}</li>)}
+      <li key={}>{}</li>
     </ul>
   );
 }
